3f8392d825ce1e9052548e5e667cb4838290ce52
EdG570 <cole570@hotmail.com>
2018-02-05 11:26:42 -0500

    Add Order and OrderItem models

---
+++ FoodXPress.Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace FoodXPress.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ FoodXPress.Models/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FoodXPress.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual FoodItem FoodItem { get; set; } 
+        public virtual Order Order { get; set; }
+    }
+}
