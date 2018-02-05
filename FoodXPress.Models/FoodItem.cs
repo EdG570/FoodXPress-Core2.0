a143f3768926e539b33f9ce9aaa2094f7c124f96
EdG570 <cole570@hotmail.com>
2018-02-05 11:09:08 -0500

    Add FoodItem model

---
+++ FoodXPress.Models/FoodItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FoodXPress.Models
+{
+    public enum Category
+    {
+        Drink,
+        Meal,
+        Appetizer,
+        Side,
+        Dessert
+    }
+
+    public class FoodItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+    }
+}
